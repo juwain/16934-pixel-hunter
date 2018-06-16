069ea89149d531634985ec50cd7dfa69ecb4cc6e
Zyuzin Vitaly <juwain@yandex.ru>
2018-06-17 00:41:58 +0300

    Добавляет модуль статистики на экране игры

---
+++ js/stats.js
@@ -0,0 +1,21 @@
+import {createElementFromTemplate} from './util.js';
+
+const createStats = () => createElementFromTemplate(`<div class="stats">
+  <ul class="stats">
+    <li class="stats__result stats__result--wrong"></li>
+    <li class="stats__result stats__result--slow"></li>
+    <li class="stats__result stats__result--fast"></li>
+    <li class="stats__result stats__result--correct"></li>
+    <li class="stats__result stats__result--unknown"></li>
+    <li class="stats__result stats__result--unknown"></li>
+    <li class="stats__result stats__result--unknown"></li>
+    <li class="stats__result stats__result--unknown"></li>
+    <li class="stats__result stats__result--unknown"></li>
+    <li class="stats__result stats__result--unknown"></li>
+  </ul>
+</div>`);
+
+export const renderStats = (container) => {
+  container.insertAdjacentElement(`beforeend`, createStats());
+};
+
