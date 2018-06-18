f49e942b18e6e6d7daf77fe3e1f5e3e324505f03
Zyuzin Vitaly <juwain@yandex.ru>
2018-06-18 23:15:06 +0300

    Добалвяет логиру смены экарнов игры в зависимости от данных

--- js/game.js
+++ js/game.js
@@ -6,116 +6,123 @@ import {renderHeader} from './header.js';
 import {renderStats} from './stats.js';
 import {gameState} from './data.js';
 
-let html;
-
-const createGameLevel = (levels) => {
-  for (let level of levels) {
-    switch (level.type) {
-      case `single`:
-        html = createElementFromTemplate(`<div class="game">
-          <p class="game__task">${level.question}</p>
-          <form class="game__content  game__content--wide">
-            <div class="game__option">
-              <img src="${level.answers[0].url}" alt="Option 1" width="705" height="455">
-              <label class="game__answer  game__answer--photo">
-                <input name="question1" type="radio" value="photo">
-                <span>Фото</span>
-              </label>
-              <label class="game__answer  game__answer--wide  game__answer--paint">
-                <input name="question1" type="radio" value="paint">
-                <span>Рисунок</span>
-              </label>
-            </div>
-          </form>
-        </div>
-        ${footerTemplate}`);
-        break;
-
-      case `double`:
-        html = createElementFromTemplate(`<div class="game">
-          <p class="game__task">${level.question}</p>
-          <form class="game__content">
-            <div class="game__option">
-              <img src="${level.answers[0].url}" alt="Option 1" width="468" height="458">
-              <label class="game__answer game__answer--photo">
-                <input name="question1" type="radio" value="photo">
-                <span>Фото</span>
-              </label>
-              <label class="game__answer game__answer--paint">
-                <input name="question1" type="radio" value="paint">
-                <span>Рисунок</span>
-              </label>
-            </div>
-            <div class="game__option">
-              <img src="${level.answers[1].url}" alt="Option 2" width="468" height="458">
-              <label class="game__answer  game__answer--photo">
-                <input name="question2" type="radio" value="photo">
-                <span>Фото</span>
-              </label>
-              <label class="game__answer  game__answer--paint">
-                <input name="question2" type="radio" value="paint">
-                <span>Рисунок</span>
-              </label>
-            </div>
-          </form>
-        </div>
-        ${footerTemplate}`);
-        break;
-
-      case `triple`:
-        html = createElementFromTemplate(`<div class="game">
-          <p class="game__task">${level.question}</p>
-          <form class="game__content  game__content--triple">
-            <div class="game__option">
-              <img src="${level.answers[0].url}" alt="Option 1" width="304" height="455">
-            </div>
-            <div class="game__option">
-              <img src="${level.answers[1].url}" alt="Option 1" width="304" height="455">
-            </div>
-            <div class="game__option">
-              <img src="${level.answers[2].url}" alt="Option 1" width="304" height="455">
-            </div>
-          </form>
-        </div>
-        ${footerTemplate}`);
-        break;
-    }
-  }
+const createGameContainer = () => {
+  return createElementFromTemplate(`<div class="game"></div>${footerTemplate}`);
 };
 
-// game single
-// const formGame = html.querySelector(`.game__content`);
+const fillGameLevel = (level) => {
+  let html;
+  switch (level.type) {
+    case `single`:
+      html = createElementFromTemplate(`
+        <p class="game__task">${level.question}</p>
+        <form class="game__content  game__content--wide">
+          <div class="game__option">
+            <img src="${level.answers[0].url}" alt="Option 1" width="705" height="455">
+            <label class="game__answer  game__answer--photo">
+              <input name="question1" type="radio" value="photo">
+              <span>Фото</span>
+            </label>
+            <label class="game__answer  game__answer--wide  game__answer--paint">
+              <input name="question1" type="radio" value="paint">
+              <span>Рисунок</span>
+            </label>
+          </div>
+        </form>`);
+      break;
 
-// formGame.addEventListener(`input`, () => {
-//   renderScreen(game3Screen);
-//   renderBackButton(game3Screen);
-// });
+    case `double`:
+      html = createElementFromTemplate(`
+        <p class="game__task">${level.question}</p>
+        <form class="game__content">
+          <div class="game__option">
+            <img src="${level.answers[0].url}" alt="Option 1" width="468" height="458">
+            <label class="game__answer game__answer--photo">
+              <input name="question1" type="radio" value="photo">
+              <span>Фото</span>
+            </label>
+            <label class="game__answer game__answer--paint">
+              <input name="question1" type="radio" value="paint">
+              <span>Рисунок</span>
+            </label>
+          </div>
+          <div class="game__option">
+            <img src="${level.answers[1].url}" alt="Option 2" width="468" height="458">
+            <label class="game__answer  game__answer--photo">
+              <input name="question2" type="radio" value="photo">
+              <span>Фото</span>
+            </label>
+            <label class="game__answer  game__answer--paint">
+              <input name="question2" type="radio" value="paint">
+              <span>Рисунок</span>
+            </label>
+          </div>
+        </form>`);
+      break;
 
-// game double
-// const formGame = html.querySelector(`.game__content`);
+    case `triple`:
+      html = createElementFromTemplate(`
+        <p class="game__task">${level.question}</p>
+        <form class="game__content  game__content--triple">
+          <div class="game__option">
+            <img src="${level.answers[0].url}" alt="Option 1" width="304" height="455">
+          </div>
+          <div class="game__option">
+            <img src="${level.answers[1].url}" alt="Option 1" width="304" height="455">
+          </div>
+          <div class="game__option">
+            <img src="${level.answers[2].url}" alt="Option 1" width="304" height="455">
+          </div>
+        </form>`);
+      break;
+  }
 
-// formGame.addEventListener(`input`, () => {
-//   const answers = Array.from(formGame.elements).filter((element) => element.checked);
+  return html;
+};
 
-//   if (answers.length === 2) {
-//     renderScreen(game2Screen);
-//     renderBackButton(game2Screen);
-//   }
-// });
+const renderGameLevel = (index) => {
+  const gameBox = gameContainer.querySelector(`.game`);
+  const level = gameData[index];
+  const gameLevel = fillGameLevel(level);
 
-// game triple
-// const formButtons = html.querySelectorAll(`.game__option`);
+  gameBox.innerHTML = ``;
+  gameBox.insertAdjacentElement(`afterbegin`, gameLevel);
 
-// Array.from(formButtons).forEach((btn) => {
-//   btn.addEventListener(`click`, () => {
-//     renderScreen(resultsScreen);
-//     renderBackButton(resultsScreen);
-//   });
-// });
 
-createGameLevel(generateLevelsData());
+  switch (level.type) {
+    case `single`:
+      gameBox.querySelector(`.game__content`).addEventListener(`input`, () => {
+        renderGameLevel(++index);
+      });
 
-renderHeader(html, gameState);
-renderStats(html.querySelector(`.game`));
+      break;
 
-export default html;
+    case `double`:
+      gameBox.querySelector(`.game__content`).addEventListener(`input`, (evt) => {
+        const answers = Array.from(evt.currentTarget.elements).filter((element) => element.checked);
+
+        if (answers.length === 2) {
+          renderGameLevel(++index);
+        }
+      });
+
+      break;
+
+    case `triple`:
+      Array.from(gameBox.querySelectorAll(`.game__option`)).forEach((btn) => {
+        btn.addEventListener(`click`, () => renderGameLevel(++index));
+      });
+
+      break;
+  }
+
+  renderStats(gameBox);
+};
+
+const gameData = generateLevelsData();
+const gameContainer = createGameContainer();
+
+renderGameLevel(0);
+renderHeader(gameContainer, gameState);
+
+export default gameContainer;
