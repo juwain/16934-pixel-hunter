3b88e264686d032be408fb1b544b7a589af9d6a3
Zyuzin Vitaly <juwain@yandex.ru>
2018-06-20 12:13:46 +0300

    Дорабатывает модуль шапки

--- js/header.js
+++ js/header.js
@@ -1,18 +1,24 @@
 import {createElementFromTemplate} from './util.js';
+import {renderBackButton} from './back-button.js';
 
-const createHeader = (data) => createElementFromTemplate(`<header class="header">
-<h1 class="game__timer">${data.time}</h1>
-<div class="game__lives">
-  ${new Array(3 - data.livesNumber)
-    .fill(`<img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">`)
-    .join(``)}
-  ${new Array(data.livesNumber)
-    .fill(`<img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">`)
-    .join(``)}
-</div>
-</header>`);
+const createHeader = (gameState) => createElementFromTemplate(`
+  <h1 class="game__timer">${gameState.time}</h1>
+  <div class="game__lives">
+    ${new Array(3 - gameState.livesNumber)
+      .fill(`<img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">`)
+      .join(``)}
+    ${new Array(gameState.livesNumber)
+      .fill(`<img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">`)
+      .join(``)}
+  </div>
+`);
 
-export const renderHeader = (container, state) => {
-  container.insertAdjacentElement(`afterbegin`, createHeader(state));
+export const renderHeader = (container, gameState) => {
+  const headerBox = container.querySelector(`header.header`);
+
+  headerBox.innerHTML = ``;
+
+  renderBackButton(headerBox);
+  headerBox.insertAdjacentElement(`afterbegin`, createHeader(gameState));
 };
 
